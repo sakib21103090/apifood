771951597b8223294e30abcbce66e28d82703a90
sakib21103090 <21103090@iubat.edu>
2023-02-26 08:41:02 +0600

    add modal and error

--- js/meal.js
+++ js/meal.js
@@ -24,10 +24,15 @@ const displayMeals= meals =>{
             }" class="card-img-top" alt="...">
             <div class="card-body">
               <h5 class="card-title">${meal.strMeal}</h5>
-              <p class="card-text">${meal.strIngredient8
-              }</p>
+              <p class="card-text">A food critic is sometimes also known as a food reviewer, a food writer, a food journalist, a food blogger or a restaurant critic</p>
+
+              <!-- Button trigger modal -->
+              <button onclick="loadMealInfo(${meal.idMeal})" type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#foodmodal">
+                More Info
+              </button>
             </div>
           </div>
+          
           `
 
           mealContainer.appendChild(mealDiv);
@@ -45,6 +50,27 @@ const searchBtn = () =>{
 const searchField=document.getElementById('search-field'). value;
   
 loadMeals(searchField);
+}
+
+const loadMealInfo = idMeal =>{
+   const url=`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${idMeal}`
+   fetch(url)
+   .then(res => res.json())
+   .then(data => mealInfoDisplay(data.meals[0]))
+   .catch(error =>console.log(error))
+ 
+}
+
+const mealInfoDisplay = meal =>{
+ const demo=document.getElementById('foodmodaltitle');
+ demo.innerText=meal.strMeal;
+
+ const img=document.getElementById('imgT');
+ img.innerHTML = `
+ <img class="img-fluid" src="${meal.strMealThumb}">
+ <h3>kha vai kha</h3>
+ 
+ `
 }
 
 loadMeals('rice');
